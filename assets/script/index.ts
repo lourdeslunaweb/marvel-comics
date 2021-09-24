0779f8940b248c7866ad8f66e4991afb5b70cca1
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-09-24 20:19:51 -0300

    create type Card, separte functions from fetch function, check and fix paginations functions

--- assets/script/index.ts
+++ assets/script/index.ts
@@ -2,21 +2,22 @@
 // *** Display cards in index.html ***
 // ***********************************
 
-// Params Index
+// *** Params Index ***
 const paramsIndex = new URLSearchParams(window.location.search);
 
-// Set variables
+// *** Set variables ***
 const baseUrl: string = "https://gateway.marvel.com:443/v1/public/";
 const apiKey: string = "06e295e3c238e43e31ef140c424be15b";
 const hash: string = "1eee8ff490d4a973b65d6f613e9569ff";
 const limit = 20;
 let offset = limit * (Number(paramsIndex.get('page')));
+// let typeUrl = paramsIndex.get('type');
 let contentHTML = "";
 let totalPages;
 
-// Nodes
+// *** Nodes ***
 const typeFilter = document.getElementById("type-filter");
-const typeUrl = typeFilter.value;
+// const typeUrl = typeFilter.value;
 const searchInput = document.getElementById("search-input");
 const results = document.getElementById("results");
 const older = document.getElementById("older");
@@ -30,50 +31,59 @@ const nextBtn = document.getElementById("next-btn");
 const lastPageBtn = document.getElementById("last-page-btn");
 const anchorLastPageBtn = document.getElementById("anchor-last-page-btn");
 
-// Refresh cards table function
-const refreshCardsTable = (offset: number, type: string) => {
+// *** Fetch Function of Index ***
+const fetchFunction = (offset: number, type: string) => {
     contentHTML = "";
     const urlAPI = `${baseUrl}${type}?ts=1&apikey=${apiKey}&hash=${hash}&limit=${limit}&offset=${offset}`;
     fetch(urlAPI)
         .then(res => res.json())
         .then((json) => {
-            const totalResults = json.data.total ? json.data.total : "No hay resultados";
-            totalPages = (Math.ceil(totalResults/limit))-1;
-            anchorLastPageBtn.setAttribute("href", `./index.html?page=${totalPages}`);
-            showHiddeBtn(totalResults, offset, limit);
-            results.innerText = `${totalResults} resultados`;
+            const totalResults = json.data.total ? json.data.total : 0;
             const cards = json.data.results;
-            for (const card of cards) {
-                let thumb = card.thumbnail ? card.thumbnail : "";
-                let cardTitle = card.title ? card.title : card.name;
-                let hrefData = `./data.html?type=${type}&id=${card.id}`;
-                contentHTML += `
-            <div class="card-div">
-                <a href="${hrefData}">
-                    <img src="${thumb.path}.${thumb.extension}" alt="${cardTitle}"  class="card-home">
-                </a>
-                <h3>${cardTitle}</h3>
-            </div>`;
-            }
-            marvelCards.innerHTML = contentHTML;
+            displayTotalResults(totalResults, results);
+            lastPage(totalResults, limit);
+            showHiddeBackwardBtn(totalResults, offset, limit);
+            displayCards(cards, type)
         });
 }
-refreshCardsTable(offset, typeFilter.value);
 
-// Refresh cards table by types (comics or characters)
+// *** Display Marvel Cards ***
+const displayCards = (cards: Card[], type: string) => {
+    for (const card of cards) {
+        let thumbPath = card.thumbnail.path ? card.thumbnail.path : "https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953"
+        let thumbExtension = card.thumbnail.extension ? card.thumbnail.extension : "jpg"
+        let cardTitle = card.title ? card.title : card.name;
+        let hrefData = `./data.html?type=${type}&id=${card.id}`;
+        contentHTML += `
+    <div class="card-div">
+        <a href="${hrefData}">
+            <img src="${thumbPath}.${thumbExtension}" alt="${cardTitle}"  class="card-home">
+        </a>
+        <h3>${cardTitle}</h3>
+    </div>`;
+    }
+    marvelCards.innerHTML = contentHTML;
+}
+
+// *** Display Totals Results of fetch ***
+const displayTotalResults = (result: number, node) => {
+    node.innerText = `${result} resultados`;
+}
+
+
+// *** Refresh cards table by types (comics or characters) ***
 const refreshTablesByTypes = () => {
     if (typeFilter.value === "comics") {
-        refreshCardsTable(offset, "comics")
+        fetchFunction(offset, "comics")
     } else if (typeFilter.value === "characters") {
-        refreshCardsTable(offset, "characters")
+        fetchFunction(offset, "characters")
     }
 }
 typeFilter.addEventListener("change", refreshTablesByTypes);
 
-// *******************************
-// *** Pagination in index.html***
-// *******************************
-// Note: Last Page is setted into refreshCardsTable with an anchor in anchorLastPageBtn
+// **********************************
+// *** Pagination Btn in index.html***
+// **********************************
 
 const nextPage = () => {
     let page = Number(paramsIndex.get('page'));
@@ -86,6 +96,11 @@ const nextPage = () => {
 }
 nextBtn.addEventListener("click", nextPage);
 
+const lastPage = (result: number, limit: number) => {
+    totalPages = (Math.ceil(result / limit)) - 1;
+    anchorLastPageBtn.setAttribute("href", `./index.html?page=${totalPages}`);
+}
+
 const prevPage = () => {
     let page = Number(paramsIndex.get('page'));
     if (!page) {
@@ -97,23 +112,14 @@ const prevPage = () => {
 }
 prevBtn.addEventListener("click", prevPage);
 
-const firstPage = () =>{
+const firstPage = () => {
     paramsIndex.set('page', (0).toString());
     window.location.href = 'index.html?' + paramsIndex;
 }
 firstPageBtn.addEventListener("click", firstPage);
 
-// Show or hidde fordward btn and backward btn
-
-const showHiddeBtn = (results : number, offset: number, limit: number) =>{
-    if (results === offset + limit) {
-        nextBtn.classList.add("hidden");
-        lastPageBtn.classList.add("hidden");
-    } else {
-        nextBtn.classList.remove("hidden");
-        lastPageBtn.classList.remove("hidden");
-    }
-
+// *** Show or hidde Barckward Btn (firstPageBtn and prevBtn) ***
+const showHiddeBackwardBtn = (results: number, offset: number, limit: number) => {
     if (offset === 0) {
         prevBtn.classList.add("hidden");
         firstPageBtn.classList.add("hidden");
@@ -123,14 +129,37 @@ const showHiddeBtn = (results : number, offset: number, limit: number) =>{
     }
 }
 
+// *** Show or Hidde Forward Btn (lastPageBtn and nextBtn) ***
+// const showHiddeFordwardBtn = (finalPage) =>{
+// getparam(page)
+// if page === finalPage
+//         if (results === offset + limit) {
+//         nextBtn.classList.add("hidden");
+//         lastPageBtn.classList.add("hidden");
+//     } else {
+//         nextBtn.classList.remove("hidden");
+//         lastPageBtn.classList.remove("hidden");
+//     }
+// }
+
+
 // **************************************
 // *** Initial function of index.html ***
 // **************************************
 
-// const initIndex = () => {
-    
-// }
-// initIndex()
+const initIndex = () => {
+    // let page = Number(paramsIndex.get('page'));
+    // let typeAux = paramsIndex.get('type');
+    // if (!typeAux) {
+    //     paramsIndex.set('type', typeFilter.value)
+    // }
+    // if (!page) {
+    //     paramsIndex.set('page', '0')
+    // }
+    // window.location.href = 'index.html?' + paramsIndex;
+    fetchFunction(offset, typeFilter.value)
+}
+initIndex()
 
 
 
